83c2446b171359e0c5d66a9d72b2e2deda5a9c55
Diego Collares <diego.collares@gmail.com>
2025-02-09 17:28:12 -0300

    Add tailwind and configure

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source 'https://rubygems.org'
 
 gem 'rackup', '~> 2.2'
 gem 'sinatra'
+
+gem "sinatra-tailwind", "~> 0.2.1"
--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/tailwind'
+
+register Sinatra::Tailwind
 
 get '/' do
   erb :index
